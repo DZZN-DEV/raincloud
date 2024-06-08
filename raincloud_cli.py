1b21f44cfd563dbb28c545b846bbf1b79d7f22bc
DZZN-DEV <93328813+DZZN-DEV@users.noreply.github.com>
2024-06-09 01:29:51 +0200

    Update raincloud_cli.py

--- raincloud_cli.py
+++ raincloud_cli.py
@@ -1,25 +1,49 @@
 import argparse
 from raincloud import SCTrack, SCSet
 import os
+import urllib.request
+import regex
+import json
 
-client_id_filepath = 'client_id.txt'
-with open(client_id_filepath, 'r') as client_id_txt:
-    client_id = client_id_txt.read().strip()
+limit = 1000
 
-assert(client_id != "PASTE SOUNDCLOUD CLIENT ID (AND NOTHING ELSE) HERE."), "brother please add your client ID to client_id.txt or use the command line argument"
+def fetch_clientid():
+    agent = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Mobile/15E148 Safari/604.1"
+    headers = {"User-Agent": agent}
+    req = urllib.request.Request("https://m.soundcloud.com", headers=headers)
+    with urllib.request.urlopen(req) as response:
+        html = response.read().decode()
+    match = regex.search(r'"clientId":"([0-9a-zA-Z\-_]{32})",', html)
+    if match:
+        return match.group(1)
+    else:
+        raise ValueError("Unable to fetch client ID")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description = "simple soundcloud downloader")
-    parser.add_argument('sc_url', type = str, help = "soundcloud URL")
-    parser.add_argument('--cid', type = str, default = client_id, help = "soundcloud client ID, can be obtained via F12 on refresh.")
-    parser.add_argument('--nm', default = False, action = 'store_true', help = 'just download mp3, no metadata')
+    parser = argparse.ArgumentParser(description="Simple SoundCloud downloader")
+    parser.add_argument('sc_url', type=str, help="SoundCloud URL")
+    parser.add_argument('--cid', type=str, default=None, help="SoundCloud client ID, can be obtained via F12 on refresh.")
+    parser.add_argument('--nm', default=False, action='store_true', help='Just download mp3, no metadata')
     args = parser.parse_args()
 
-    os.makedirs('dls', exist_ok = True)
+    if args.cid is None:
+        try:
+            client_id = fetch_clientid()
+        except ValueError as e:
+            print(e)
+            exit(1)
+    else:
+        client_id = args.cid
+
+    os.makedirs('dls', exist_ok=True)
     try:
-        sc = SCTrack(args.cid, args.sc_url)
+        sc = SCTrack(client_id, args.sc_url)
         stream_url = sc.stream_url
         sc.stream_download('dls', not args.nm)
     except AssertionError as e:
         print(e)
         cont = input('Playlist/set detected. Would you like to download all? (Y/n)')
+        if cont.lower() == 'y':
+            sc_set = SCSet(client_id, args.sc_url)
+            for track in sc_set.tracks:
+                track.stream_download('dls', not args.nm)
